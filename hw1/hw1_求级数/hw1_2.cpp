a685f05a71663f58b026a827b5164128d0818cc8
Brian Bai <1980337991@qq.com>
2024-10-03 14:33:40 +0800

    gpt ver2 成功

--- hw1/hw1_求级数/hw1_2.cpp
+++ hw1/hw1_求级数/hw1_2.cpp
@@ -2,64 +2,61 @@
 #include <vector>
 using namespace std;
 
-struct num {
-    vector<char> elem;
-};
 
-void L_increace(num& L) {
-    L.elem.push_back(0);
+void L_increace(vector<char> &L) {
+    L.push_back(0);
 }
 
-void L_give(num L_src, num& L_dst) {
-    L_dst.elem.resize(L_src.elem.size());
-    for (size_t i = 0; i < L_src.elem.size(); ++i) {
-        L_dst.elem[i] = L_src.elem[i];
+void L_give(vector<char> L_src, vector<char> &L_dst) {
+    L_dst.resize(L_src.size());
+    for (int i = 0; i < L_src.size(); ++i) {
+        L_dst[i] = L_src[i];
     }
 }
 
-void Int_to_digit(int I, num& L) {
+void Int_to_digit(int I, vector<char> &L) {
     while (I) {
-        L.elem.push_back(I % 10);
+        L.push_back(I % 10);
         I /= 10;
     }
 }
 
-num L_multiply(num m1, num m2) {
-    num result;
-    result.elem.resize(m1.elem.size() + m2.elem.size(), 0);
-    for (size_t i = 0; i < m1.elem.size(); i++) {
-        for (size_t j = 0; j < m2.elem.size(); j++) {
-            result.elem[i + j] += m1.elem[i] * m2.elem[j];
-            if (result.elem[i + j] >= 10) {
-                result.elem[i + j + 1] += result.elem[i + j] / 10;
-                result.elem[i + j] %= 10;
+vector<char> L_multiply(vector<char> m1, vector<char> m2) {
+    vector<char> result;
+    result.resize(m1.size() + m2.size(), 0);
+    for (size_t i = 0; i < m1.size(); i++) {
+        for (size_t j = 0; j < m2.size(); j++) {
+            result[i + j] += m1[i] * m2[j];
+            if (result[i + j] >= 10) {
+                result[i + j + 1] += result[i + j] / 10;
+                result[i + j] %= 10;
             }
         }
     }
 
-    while (result.elem.size() > 1 && result.elem.back() == 0) {
-        result.elem.pop_back();
-    }
+    while (result.size() > 1 && result.back() == 0)
+        result.pop_back();
 
     return result;
 }
 
-num L_add(num a1, num a2) {
-    num result;
-    result.elem.resize(max(a1.elem.size(), a2.elem.size()) + 1, 0);
-    size_t i = 0, carry = 0;
+vector<char> L_add(vector<char> a1, vector<char> a2) {
+    vector<char> result;
+    result.resize(max(a1.size(), a2.size()) + 1, 0);
+    unsigned int i = 0;
+    char carry = 0;
 
-    while (i < a1.elem.size() || i < a2.elem.size() || carry) {
-        if (i < a1.elem.size()) carry += a1.elem[i];
-        if (i < a2.elem.size()) carry += a2.elem[i];
-        result.elem[i] = carry % 10;
+    while (i < a1.size() || i < a2.size() || carry) {
+        if (i < a1.size()) 
+            carry += a1[i];
+        if (i < a2.size()) carry += a2[i];
+        result[i] = carry % 10;
         carry /= 10;
         i++;
     }
 
-    while (result.elem.size() > 1 && result.elem.back() == 0) {
-        result.elem.pop_back();
-    }
+    while (result.size() > 1 && result.back() == 0)
+        result.pop_back();
 
     return result;
 }
@@ -68,29 +65,35 @@ int main() {
     int N, A;
     cin >> N >> A;
 
-    num sum, now, E;
-    sum.elem.push_back(0);
-    E.elem.clear();
+    vector<char> sum(1,0), now(1,0), E, exp;
     Int_to_digit(A, E);
-
-    now.elem.clear();
+    Int_to_digit(A, exp);
 
     for (int i = 1; i <= N; i++) {
-        num F;
+        vector<char> F;
         Int_to_digit(i, F);
 
-        if (i > 1) {
-            E = L_multiply(E, E);
-        }
-
-        now = L_multiply(E, F);
-        sum = L_add(sum, now);
+        if (i > 1)
+            L_give(L_multiply(exp, E), exp);
+        //exp = L_multiply(exp, E);
+        /*for (int i = exp.size()-1; i >= 0; i--)
+            cout << char('0' + exp[i]);
+        cout << endl;*/        
+        L_give(L_multiply(exp, F), now);
+        //now = L_multiply(exp, F);
+        /*for (int i = now.size() - 1; i >= 0; i--)
+            cout << char('0' + now[i]);
+        cout << endl;*/
+        L_give(L_add(sum, now), sum);
+        //sum = L_add(sum, now);
+        /*for (int i = sum.size() - 1; i >= 0; i--)
+            cout << char('0' + sum[i]);
+        cout << endl;*/
     }
 
     // ÄæÐòÊä³ösumÊý×é
-    for (int i = sum.elem.size() - 1; i >= 0; i--) {
-        cout << static_cast<int>(sum.elem[i]);
-    }
+    for (int i = sum.size() - 1; i >= 0; i--)
+        cout << static_cast<int>(sum[i]);
     cout << endl;
 
     return 0;
