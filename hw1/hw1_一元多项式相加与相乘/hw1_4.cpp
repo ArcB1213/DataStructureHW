177d2785a0318c86434e411390149297c19d097b
Brian Bai <1980337991@qq.com>
2024-10-04 13:21:31 +0800

    ver1.1多项式加法实现

---
+++ hw1/hw1_一元多项式相加与相乘/hw1_4.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+using namespace std;
+
+struct Polynomial
+{
+	int coef;
+	int expn;
+	Polynomial* next;
+};
+
+Polynomial* CreateNode(Polynomial* p,int coef,int expn)
+{
+	Polynomial* q = (Polynomial*)malloc(sizeof(Polynomial));
+	p->next = q;
+	q->next = NULL;
+	q->coef = coef;
+	q->expn = expn;
+	p = q;
+	return p;
+}
+
+Polynomial* add_polyn(Polynomial *P1,Polynomial *P2)
+{
+	Polynomial* Psum;
+	Polynomial* pa = P1, * pb = P2, * ps= (Polynomial*)malloc(sizeof(Polynomial));
+	Psum = ps;
+	ps->next = NULL;
+	Polynomial* paCur = pa->next, * pbCur = pb->next;
+	while (paCur && pbCur) {
+		if (paCur->expn < pbCur->expn) {
+			ps = CreateNode(ps, paCur->coef, paCur->expn);
+			paCur = paCur->next;
+		}
+		else if (paCur->expn == pbCur->expn) {
+			int sum=paCur->coef + pbCur->coef;
+			if (sum)
+				ps = CreateNode(ps, sum, paCur->expn);
+			paCur = paCur->next;
+			pbCur = pbCur->next;
+		}
+		else if (paCur->expn > pbCur->expn) {
+			ps = CreateNode(ps, pbCur->coef, pbCur->expn);
+			pbCur = pbCur->next;
+		}
+	}
+	while (paCur) {
+		ps = CreateNode(ps, paCur->coef, paCur->expn);
+		paCur = paCur->next;
+	}
+	while (pbCur) {
+		ps = CreateNode(ps, pbCur->coef, pbCur->expn);
+		pbCur = pbCur->next;
+	}
+	return Psum;
+}
+
+int main()
+{
+	int n, m;
+	Polynomial *P1, *P2;
+	cin >> m;
+	Polynomial* pa=(Polynomial *)malloc(sizeof(Polynomial));//头节点
+	P1 = pa;
+	pa->next = NULL;
+	Polynomial* paCur = pa;//指向P1的当前节点
+	for (int i = 1; i <= m; i++) {
+		Polynomial* p = (Polynomial*)malloc(sizeof(Polynomial));
+		paCur->next = p;
+		paCur = p;
+		p->next = NULL;
+		cin >> p->coef >> p->expn;
+	}
+	cin >> n;
+	Polynomial* pb = (Polynomial*)malloc(sizeof(Polynomial));//头节点
+	P2 = pb;
+	pb->next = NULL;
+	Polynomial* pbCur = pb;//指向P1的当前节点
+	for (int i = 1; i <= n; i++) {
+		Polynomial* p = (Polynomial*)malloc(sizeof(Polynomial));
+		pbCur->next = p;
+		pbCur = p;
+		p->next = NULL;
+		cin >> p->coef >> p->expn;
+	}
+
+	/*cout << "P1=";
+	paCur = pa->next;
+	while (paCur) {
+		cout << paCur->coef << ' ' << paCur->expn << ' ';
+		paCur = paCur->next;
+	}
+	cout << endl;*/
+
+	/*cout << "P2=";
+	pbCur = pb->next;
+	while (pbCur) {
+		cout << pbCur->coef << ' ' << pbCur->expn << ' ';
+		pbCur = pbCur->next;
+	}
+	cout << endl;*/
+
+	int opr;
+	cin >> opr;
+	if (opr == 0) {
+		Polynomial* Psum = add_polyn(P1, P2);
+		Polynomial* qs = Psum->next;
+		while (qs) {
+			cout << qs->coef << ' ' << qs->expn << ' ';
+			qs = qs->next;
+		}
+		cout << endl;
+	}
+	return 0;
+}
