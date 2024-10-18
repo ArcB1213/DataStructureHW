e44ecce3a70fe3bffc9629dbe43f459d3990342c
Brian Bai <1980337991@qq.com>
2024-10-18 21:13:35 +0800

    hw2_1 ver1

---
+++ hw2/hw2列车进站/hw2_1.cpp
@@ -0,0 +1,172 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct train {
+	char name;
+	int status;
+};
+
+struct Trains {
+	train* t;
+	int length;
+	int size;
+};
+
+struct Stack {
+	train* wait;
+	int size;
+};
+
+int FindElem(char src, train L[], int length)
+{
+	int i = 0;
+	for (; i < length; i++) {
+		if (L[i].name == src)
+			break;
+	}
+	if (i < length)
+		return i;
+	else
+		return -1;
+}
+
+int delete_head(Trains &T)
+{
+	for (int i = 0; i < T.length - 1; i++)
+		T.t[i] = T.t[i + 1];
+	T.length--;
+	T.size--;
+	if (T.length > 0) {
+		train* tp = (train*)realloc(T.t, (T.size) * sizeof(train));
+		if (tp == NULL)
+			return -1;
+		T.t = tp;
+	}
+	else
+		free(T.t);
+	return 0;
+}
+
+int Push_Stack(Stack& S,train elem)
+{
+	S.size++;
+	train* sp = (train*)realloc(S.wait, (S.size) * sizeof(train));
+	if (sp == NULL)
+		return -1;
+	S.wait = sp;
+
+	S.wait[S.size - 1] = elem;
+
+	return 0;
+}
+
+int Pop_Stack(Stack& S)
+{
+	S.size--;
+	if (S.size > 0) {
+		train* sp = (train*)realloc(S.wait, (S.size) * sizeof(train));
+		if (sp == NULL)
+			return -1;
+		S.wait = sp;
+	}
+	else
+		free(S.wait);
+
+	return 0;
+}
+
+void copy_trains(const Trains src, Trains& dst)
+{
+	dst.t = (train*)malloc(src.length * sizeof(train));
+	dst.length = src.length;
+	dst.size = src.size;
+	for (int i = 0; i < src.length; i++) {
+		dst.t[i] = src.t[i];
+	}
+}
+
+int main()
+{
+	Trains Init;
+	Init.t = (train*)malloc(3 * sizeof(train));
+	Init.size = 3;
+	Init.length = 0;
+	
+	//输入列车顺序
+	string ini_line;
+	cin >> ini_line;
+	int ini_len = ini_line.length();
+	Init.size = ini_len;
+	Init.length = ini_len;
+	Init.t = (train*)malloc(Init.size * sizeof(train));
+	for (int i = 0; i < Init.length; i++) {
+		Init.t[i].name = ini_line[i];
+		Init.t[i].status = 0;
+	}
+
+/*	while (1) {
+		char temp = _getche();
+		if (temp != '\r') {
+			Init.length++;
+			if (Init.length > Init.size) {
+				Init.size = Init.length;
+				train* tp = (train*)realloc(Init.t, (Init.size) * sizeof(train));
+				if (tp == NULL)
+					return -1;
+				Init.t = tp;			
+			}
+			Init.t[Init.length - 1].name = temp;
+			Init.t[Init.length - 1].status = 0;
+		}
+		else
+			break;
+	}
+
+*/
+	string out;
+	while (cin >> out) {
+		Trains Temp;
+		copy_trains(Init, Temp);
+
+		//输入列车出站顺序
+		//char *out=(char*)malloc((Init.length+1)*sizeof(char));
+		//cin >> out;
+
+		string result = "yes";
+		Stack train_w;
+		train_w.wait = NULL;
+		train_w.size = 0;
+		for (int i = 0; i < Init.length; i++) {
+			if (Temp.t[0].name == out[i]) {
+				delete_head(Temp);
+			}
+			else {
+				//在原队列里寻找该车辆
+				int f1 = FindElem(out[i], Temp.t, Temp.length);
+				if (f1 >= 0) {
+					while (Temp.t[0].name != out[i]) {
+						Push_Stack(train_w, Temp.t[0]);
+						delete_head(Temp);
+					}
+					delete_head(Temp);
+				}
+				//原队列没找到，到栈里找
+				else {
+					if (train_w.wait[train_w.size - 1].name == out[i])
+						Pop_Stack(train_w);
+					else
+						result = "no";
+				}
+			}
+		}
+		cout << result << endl;
+		if (train_w.size)
+			free(train_w.wait);
+		if (Temp.size)
+			free(Temp.t);
+		//free(out);
+	}
+	
+	return 0;
+}
